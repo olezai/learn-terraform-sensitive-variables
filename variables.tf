9aa6648b71944adc667011d178181c79340c9e57
Oleksii Zaivenko <alexey.zaivenko@gmail.com>
2025-05-03 14:06:49 +0000

    refactoring secure variables

--- variables.tf
+++ variables.tf
@@ -55,3 +55,14 @@ variable "instance_type" {
   default     = "t2.micro"
 }
 
+variable "db_username" {
+  description = "Database administrator username"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "Database administrator password"
+  type        = string
+  sensitive   = true
+}
